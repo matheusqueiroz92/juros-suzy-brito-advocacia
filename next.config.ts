dbcf4df6918fa051ac62de38272f95e956c85b18
matheusqueiroz92 <matheus_giga@hotmail.com>
2025-07-23 11:35:42 -0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Melhora a compatibilidade em produção
+  images: {
+    domains: ["suzibrito.com.br"], // Se você estiver usando o componente Next Image
+    unoptimized: true, // Pode ajudar com problemas de otimização
+  },
+  // Configuração que explicita o caminho base para os arquivos públicos
+  assetPrefix: "",
+  basePath: "",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
